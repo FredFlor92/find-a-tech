aad4df69cf5e1bfb1561d2ab42f27d56e60cd662
freddyflorial@gmail.com <freddyflorial@gmail.com>
2021-07-04 13:31:04 -0400

    establish session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+    def new
+    end 
+
+    def create 
+        @user = User.find_by(:username => params[:username])
+        @user = User.find_by(:email => params[:email])
+        if @user
+             
+        else 
+            redirect_to "/login", :notice => "User not found"
+        end 
+
+    end 
+
+end
