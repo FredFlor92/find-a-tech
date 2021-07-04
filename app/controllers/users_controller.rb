47bf50396a706c2473f0197551965d4e637d48bf
freddyflorial@gmail.com <freddyflorial@gmail.com>
2021-07-04 12:40:10 -0400

    user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+    #New User Action
+    def new
+        # @user = User.new
+    end 
+    #Creates the User Action
+    def create
+        @user = User.new
+        @user.username = params[:username]
+        @user.email = params[:email]
+        #@user.password = params[:password]
+        @user.save
+
+        redirect_to "/login"
+        # @user = User.new(user_params)
+        # if @user.save
+        #     #login the user
+        #     session[:user_id] = @user.id
+        #     redirect_to user_path(@user)
+        # else
+        #     render :new 
+        # end 
+    end 
+
+
 end
